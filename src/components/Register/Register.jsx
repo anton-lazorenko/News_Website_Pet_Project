f40c89fb1e635f39a16970b0b02c9ca0a56776d7
Anton Lazorenko <153720712+anton-lazorenko@users.noreply.github.com>
2025-11-18 18:36:05 +0200

    set up the registration form to be sent to a fake server

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,14 +1,34 @@
-import './register.css'
+import './register.css';
 
-import closeIcon from '../../assets/close-icon.svg'
-import { UniversalButton } from '../../components/UniversalButton'
- 
+import closeIcon from '../../assets/close-icon.svg';
+import { UniversalButton } from '../../components/UniversalButton';
+import { useState } from 'react';
 
-export function Register({ onClose }) { 
+export function Register({ onClose }) {
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ firstName, lastName, email, pass })
+    });
+
+    const data = await res.json();
+    console.log("REGISTER RESULT:", data); 
+
+     onClose();
+  }
 
   return (
     <div>
-      <form  className="register-form" onSubmit={(e) => e.preventDefault()}>
+      <form className="register-form" onSubmit={handleSubmit}>
         <fieldset className='register-items'>
           <legend>Register</legend>
 
@@ -19,6 +39,9 @@ export function Register({ onClose }) {
               className="register-input"
               id="firstName"
               name="firstName"
+              placeholder="John"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
             />
           </label>
 
@@ -29,6 +52,9 @@ export function Register({ onClose }) {
               className="register-input"
               id="lastName"
               name="lastName"
+              placeholder="Doe"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
             />
           </label>
 
@@ -39,11 +65,31 @@ export function Register({ onClose }) {
               className="register-input"
               id="email"
               name="email"
+              placeholder="@gmail.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+
+          <label htmlFor="pass">
+            Password
+            <input
+              type="password"
+              className="register-input"
+              id="pass"
+              name="pass"
+              placeholder="password"
+              value={pass}
+              onChange={(e) => setPass(e.target.value)}
             />
           </label>
         </fieldset>
+
         <UniversalButton type='submit'>Submit</UniversalButton>
-        <button type="button" className='close-button' onClick={onClose}><img className='close-icon' src={closeIcon} alt="Close" /></button>
+
+        <button type="button" className='close-button' onClick={onClose}>
+          <img className='close-icon' src={closeIcon} alt="Close" />
+        </button>
       </form>
     </div>
   )
